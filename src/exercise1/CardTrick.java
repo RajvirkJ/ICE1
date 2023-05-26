626599ff235fdabd1b6e051d93bea5659d450cb9
RajvirkJ <134092217+RajvirkJ@users.noreply.github.com>
2023-05-26 14:47:52 -0400

    Update CardTrick.java
    Personal information edited

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -55,11 +55,11 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Rajvir, but you can call me student, Raj");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
+        System.out.println("-- Be more focussed on programming");
         System.out.println("-- Have a semester with no violations of academic integrity!");
 	System.out.println();	
 
@@ -67,7 +67,7 @@ public class CardTrick {
         System.out.println("-- Investing");
         System.out.println("-- Cooking");
         System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Playing with my kid");
 
         System.out.println();
         
